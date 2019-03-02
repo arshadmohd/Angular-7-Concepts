ea59849e5e3f7d12606ebe1c662c982e03d231c0
MOHD ARSHAD <mohd.arshad8889@gmail.com>
2019-03-02 16:54:17 +0530

    reactive forms and validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -42,7 +42,8 @@ declare let jQuery;
     BrowserModule,    
     RouterModule.forRoot(appRoutes),
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [ProductService,
     CartService,
--- src/app/login-and-user/add-update-user.component.ts
+++ src/app/login-and-user/add-update-user.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { LoginService } from "../shared/services/login.service";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { formArrayNameProvider } from "@angular/forms/src/directives/reactive_directives/form_group_name";
 
 @Component({
     selector: 'updateuser',
@@ -7,6 +9,37 @@ import { LoginService } from "../shared/services/login.service";
     styleUrls : ['./add-update-user.component.css']
 })
 
-export class UserComponent{
+export class UserComponent implements OnInit{
+
+    name: FormControl;
+    age: FormControl;
+    email: FormControl;
+    gender : FormControl;
+    registrationForm: FormGroup;
     constructor(private loginService : LoginService){}
+
+    ngOnInit(){
+        this.name =  new FormControl("", [Validators.required]);
+        this.age =  new FormControl("", [Validators.required]);
+        this.gender  =new FormControl("male", [Validators.required]);
+        this.email =  new FormControl("arshad.md1989@gmail.com", [Validators.email]);
+
+        this.registrationForm =  new FormGroup({
+            name :  this.name,
+            age : this.age,
+            gender : this.gender,
+            email : this.email
+
+        });
+
+    }
+
+    submitForm(regForm){
+        console.log(regForm);
+        if(regForm.valid){
+            console.log("Form is valid");
+        }else{
+            console.log("Form is not valid");
+        }
+    }
 }
